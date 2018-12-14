86c313d25c11498b31ae3fc4f455236ecbb1f7db
Thomas Campbell <thomascampbell1@gmail.com>
2018-12-14 12:50:46 -0700

    added cos to class, getting stange compile errors

--- BinnedOverlays-Fig3/DrawFigure3.C
+++ BinnedOverlays-Fig3/DrawFigure3.C
@@ -4,11 +4,8 @@
 
 void DrawFigure3(){
 
-	stackResult* res1 = new stackResult(true, true);
-	stackResult* res2 = new stackResult(true, false);
-	stackResult* res3 = new stackResult(false, true);
-	stackResult* res4 = new stackResult(false, false);
-
+	stackResult* res1 = new stackResult();
+	res1->GetResultCos(true,true);
 	TCanvas* can = new TCanvas("can", "can", 2800, 1500);
 
     TPad* subPad = new TPad("pad", "pad", 0.1, 0.1, 1.0, 1.0);
@@ -20,17 +17,8 @@ void DrawFigure3(){
 	res1->data->Draw("same P0");
 	res1->leg->Draw("same");
 	subPad->cd(2);
-	res2->mc->Draw("hist");
-	res2->data->Draw("same P0");
-	res2->leg->Draw("same");
 	subPad->cd(3);
-	res3->mc->Draw("hist");
-	res3->data->Draw("same P0");
-	res3->leg->Draw("same");
 	subPad->cd(4);
-	res4->mc->Draw("hist");
-	res4->data->Draw("same P0");
-	res4->leg->Draw("same");
 	subPad->cd(5);
 	subPad->cd(6);
 	subPad->cd(7);
--- BinnedOverlays-Fig3/ResultClass.C
+++ BinnedOverlays-Fig3/ResultClass.C
@@ -12,7 +12,9 @@ class stackResult {
 		TH1F* data;
 		TLegend* leg;
 
-		stackResult(bool isSignal, bool isWater){
+		stackResult();
+
+		void GetResultMom(bool isSignal, bool isWater){
 			string CutStr1;
 			if(isSignal) {
 				CutStr1 = "(accum_level>4 && PIDnShowers<1 && nMuLike[0]==1";
@@ -42,20 +44,9 @@ class stackResult {
 
 
 			TTree* defaultMC = (TTree*)inFmc->Get("outTdefault");
-			//TTree* defaultMC = (TTree*)inFmc->Get("default");
 			TTree* defaultData = (TTree*)inFdata->Get("default");
 
-			//float PoTMC=351.;
-
-			//MC corrected numbers:
-			//Run5: 224.7
-			//Run6: 242.6
-			//or Data relative to 208. MC PoT
-			//Water-in: 23.80
-			//Water-out: 29.33
-			//
 			float PoTScale=PoTData/PoTMC;
-			//
 
 			double PBins[9]={0,400,530,670,800,1000,1380,2010,3410};
 			TString PBinsStr[9]={"0","400","530","670","800","1000","1380","2010","3410"};
@@ -189,5 +180,156 @@ class stackResult {
 
 			if(data->GetMaximum()>=mc->GetMaximum()) mc->SetMaximum(1.2*(data->GetMaximum()));
 			else mc->SetMaximum(1.2*(mc->GetMaximum()));
-		}
+		};
+
+		/************************************* Cosinus now.... ************************************************/
+		void GetResultCos(bool isSignal, bool isWater){
+		/******************************************************************************************************/
+
+			TFile* inFmc;
+			TFile* inFdata;
+			float PoTData;
+			float PoTMC;
+
+			if(isWater){
+				inFmc = new TFile("/Volumes/ThomasDrive/p0dCCAnalysis/Selection/DataMCOverlays/RootFiles/BSTunedFullRun5WaterIn.root", "OPEN");
+				inFdata = new TFile("/Volumes/ThomasDrive/p0dCCAnalysis/Selection/DataMCOverlays/RootFiles/FullWaterInData.root", "OPEN");
+				PoTData=28.7;
+				PoTMC=208;
+			} else {
+				inFmc = new TFile("/Volumes/ThomasDrive/p0dCCAnalysis/Selection/DataMCOverlays/RootFiles/BSTunedFullRun6Air.root", "OPEN");
+				inFdata = new TFile("/Volumes/ThomasDrive/p0dCCAnalysis/Selection/DataMCOverlays/RootFiles/FullRun6AirData.root", "OPEN");
+				PoTData=34.2;
+				PoTMC=209;
+			}
+
+			TTree* defaultMC = (TTree*)inFmc->Get("outTdefault");
+			TTree* defaultData = (TTree*)inFdata->Get("default");
+
+			float PoTScale=PoTData/PoTMC;
+
+			double PBins[9]={0,400,530,670,800,1000,1380,2010,3410};
+			TString PBinsStr[9]={"0","400","530","670","800","1000","1380","2010","3410"};
+
+			int nCosBins[8]={9,2,3,3,3,3,3,2};
+			double CosBins[9][10]={
+				{0.6,0.8,0.85,0.88,0.90,0.92,0.94,0.96,0.98,1.0},
+				{},
+				{},
+				{},
+				{},
+				{},
+				{},
+				{},
+				{}
+			};
+
+			string CutStr1 = "";
+			if(isSignal){
+				CutStr1 = "(accum_level>4 && PIDnShowers<1 && nMuLike[0]==1" ;
+			} else {
+				CutStr1 = "(accum_level>4 && PIDnShowers<1 && nMuLike[0]==2";
+			}
+
+			int pBinDraw=0;
+
+			TH1F* SigMomMC0 = new TH1F("SigMomMC0","",nCosBins[pBinDraw],CosBins[pBinDraw]);
+			TH1F* SigMomMC1 = new TH1F("SigMomMC1","",nCosBins[pBinDraw],CosBins[pBinDraw]);
+			TH1F* SigMomMC2 = new TH1F("SigMomMC2","",nCosBins[pBinDraw],CosBins[pBinDraw]);
+			TH1F* SigMomMC3 = new TH1F("SigMomMC3","",nCosBins[pBinDraw],CosBins[pBinDraw]);
+			TH1F* SigMomMC7 = new TH1F("SigMomMC7","",nCosBins[pBinDraw],CosBins[pBinDraw]);
+			TH1F* SigMomMCOther = new TH1F("SigMomMCOther","",nCosBins[pBinDraw],CosBins[pBinDraw]);
+
+
+			data = new TH1F("data","",nCosBins[pBinDraw],CosBins[pBinDraw]);
+
+			string CutStr2 = ")*weight";
+			string CutStr;
+
+			CutStr=CutStr1;
+			CutStr+=" && topology==0";
+			CutStr+=CutStr2;
+			defaultMC->Draw("selmu_costheta>>SigMomMC0",CutStr.c_str());
+
+			CutStr=CutStr1;
+			CutStr+=" && topology==1";
+			CutStr+=CutStr2;
+			defaultMC->Draw("selmu_costheta>>SigMomMC1",CutStr.c_str());
+
+			CutStr=CutStr1;
+			CutStr+=" && topology==2";
+			CutStr+=CutStr2;
+			defaultMC->Draw("selmu_costheta>>SigMomMC2",CutStr.c_str());
+
+			CutStr=CutStr1;
+			CutStr+=" && topology==3";
+			CutStr+=CutStr2;
+			defaultMC->Draw("selmu_costheta>>SigMomMC3",CutStr.c_str());
+
+			CutStr=CutStr1;
+			CutStr+=" && topology==7";
+			CutStr+=CutStr2;
+			defaultMC->Draw("selmu_costheta>>SigMomMC7",CutStr.c_str());
+
+			CutStr=CutStr1;
+			CutStr+=" && (topology<0 || (topology>3 && topology!=7))";
+			CutStr+=CutStr2;
+			defaultMC->Draw("selmu_costheta>>SigMomMCOther",CutStr.c_str());
+
+
+			SigMomMC0->Scale(PoTScale);
+			SigMomMC1->Scale(PoTScale);
+			SigMomMC2->Scale(PoTScale);
+			SigMomMC3->Scale(PoTScale);
+			SigMomMC7->Scale(PoTScale);
+			SigMomMCOther->Scale(PoTScale);
+
+			SigMomMC0->SetFillColor(kRed-9);
+			SigMomMC1->SetFillColor(kBlue-9);
+			SigMomMC7->SetFillColor(kGreen-8);
+			SigMomMC3->SetFillColor(kOrange-4);
+			SigMomMC2->SetFillColor(kYellow-7);
+			SigMomMCOther->SetFillColor(kBlack);
+
+			mc = new THStack("mc","");
+			mc->Add(SigMomMCOther);
+			mc->Add(SigMomMC3);
+			mc->Add(SigMomMC7);
+			mc->Add(SigMomMC2);
+			mc->Add(SigMomMC1);
+			mc->Add(SigMomMC0);
+
+			CutStr=CutStr1;
+			CutStr+=CutStr2;
+			defaultData->Draw("selmu_costheta>>data",CutStr.c_str());
+
+
+			//bin width
+			for(int i=0; i<nCosBins[pBinDraw]; i++){
+				SigMomMC0->SetBinContent(i+1,(SigMomMC0->GetBinContent(i+1))/(CosBins[pBinDraw][i+1]-CosBins[pBinDraw][i]));
+				SigMomMC1->SetBinContent(i+1,(SigMomMC1->GetBinContent(i+1))/(CosBins[pBinDraw][i+1]-CosBins[pBinDraw][i]));
+				SigMomMC2->SetBinContent(i+1,(SigMomMC2->GetBinContent(i+1))/(CosBins[pBinDraw][i+1]-CosBins[pBinDraw][i]));
+				SigMomMC3->SetBinContent(i+1,(SigMomMC3->GetBinContent(i+1))/(CosBins[pBinDraw][i+1]-CosBins[pBinDraw][i]));
+				SigMomMC7->SetBinContent(i+1,(SigMomMC7->GetBinContent(i+1))/(CosBins[pBinDraw][i+1]-CosBins[pBinDraw][i]));
+				SigMomMCOther->SetBinContent(i+1,(SigMomMCOther->GetBinContent(i+1))/(CosBins[pBinDraw][i+1]-CosBins[pBinDraw][i]));
+				data->SetBinContent(i+1,(data->GetBinContent(i+1))/(CosBins[pBinDraw][i+1]-CosBins[pBinDraw][i]));
+				data->SetBinError(i+1,(data->GetBinError(i+1))/(CosBins[pBinDraw][i+1]-CosBins[pBinDraw][i]));
+			}
+
+			leg = new TLegend(0.1,0.6,0.4,0.9);
+			leg->AddEntry(SigMomMC0,"CC-0#pi","f");
+			leg->AddEntry(SigMomMC1,"CC-1#pi^{-}","f");
+			leg->AddEntry(SigMomMC2,"CC-Other","f");
+			leg->AddEntry(SigMomMC3,"BKGD","f");
+			leg->AddEntry(SigMomMC7,"OOFV","f");
+			leg->AddEntry(SigMomMCOther,"Other","f");
+			leg->AddEntry(data,"Data","lep");
+
+			data->SetMarkerStyle(8);
+			data->SetMarkerSize(1);
+			data->SetMarkerColor(kBlack);
+			data->GetXaxis()->SetTitle("sel. #mu^{+} momenutm (MeV/c)");
+
+			mc->SetMaximum(1.2*(data->GetMaximum()));
+		};
 };
