a0fc142290664a3c3803b661ca967f4168f495c3
Thomas Campbell <thomascampbell1@gmail.com>
2018-12-15 17:05:48 -0700

    Figure3 base now draws, probably needs minor tweaks

--- BinnedOverlays-Fig3/DrawFigure3.C
+++ BinnedOverlays-Fig3/DrawFigure3.C
@@ -5,28 +5,27 @@
 void DrawFigure3(){
 
 	//stackResult(bool isSignal, bool isWater, bool isMomentum){
-	stackResult* res1 = new stackResult(true,true,true);
-	stackResult* res2 = new stackResult(true,true,false);
+	stackResult** res = new stackResult*[8];
+	res[0] = new stackResult(true,true,true);
+	res[1] = new stackResult(true,false,true);
+	res[2] = new stackResult(false,true,true);
+	res[3] = new stackResult(false,false,true);
+	res[4] = new stackResult(true,true,false);
+	res[5] = new stackResult(true,false,false);
+	res[6] = new stackResult(false,true,false);
+	res[7] = new stackResult(false,false,false);
+
 	TCanvas* can = new TCanvas("can", "can", 2800, 1500);
 
     TPad* subPad = new TPad("pad", "pad", 0.1, 0.1, 1.0, 1.0);
     subPad->Divide(4, 2, 0.0001, 0.0001);
     subPad->Draw();
 
-	subPad->cd(1);
-	res1->mc->Draw("hist");
-	res1->data->Draw("same P0");
-	res1->leg->Draw("same");
-	subPad->cd(2);
-	res2->mc->Draw("hist");
-	res2->data->Draw("same P0");
-	res2->leg->Draw("same");
-	subPad->cd(3);
-	subPad->cd(4);
-	subPad->cd(5);
-	subPad->cd(6);
-	subPad->cd(7);
-	subPad->cd(8);
-
+	for(int ii=0; ii<8; ii++){
+		subPad->cd(ii+1);
+		res[ii]->mc->Draw("hist");
+		res[ii]->data->Draw("same P0");
+		res[ii]->leg->Draw("same");
+	}
 	can->Print("./plots/figure3.eps");
 }
